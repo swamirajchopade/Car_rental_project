77ec192392bd215bf61ccac5943f1cc196eca990
tejasgund02 <tejasgund02@gmail.com>
2022-04-11 21:46:33 +0530

    ##..Improve navbar stability in car insert page.

--- admin/header.php
+++ admin/header.php
@@ -29,7 +29,7 @@
   <link href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,400i,700" rel="stylesheet">
 
 </head>
-<body class="hold-transition sidebar-mini layout-fixed">
+<body class="main-header navbar navbar-expand navbar-white navbar-light">
 <div class="wrapper">
 
 <!-- Navbar -->
