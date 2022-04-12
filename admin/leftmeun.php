45c931b542b1e5cae9abf874bdb54be6e56c1258
tejasgund02 <tejasgund02@gmail.com>
2022-04-12 08:49:42 +0530

    ##..Modify Create left menu (car info/inset).

--- admin/leftmeun.php
+++ admin/leftmeun.php
@@ -1,15 +1,17 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
 
 </head>
+
 <body class="hold-transition sidebar-mini layout-fixed">
-<div class="wrapper">
-<aside class="main-sidebar sidebar-dark-primary elevation-4">
+    <div class="wrapper">
+        <aside class="main-sidebar sidebar-dark-primary elevation-4">
             <!-- Brand Logo -->
 
             <a href="index3.html" class="brand-link">
-                 <img src="dist/img/AdminLTELogo.png" alt="AdminLTE Logo" class="brand-image img-circle elevation-3" style="opacity: .8">
+                <img src="dist/img/AdminLTELogo.png" alt="AdminLTE Logo" class="brand-image img-circle elevation-3" style="opacity: .8">
                 <span class="brand-text font-weight-light">Admin Car Rental</span>
             </a>
 
@@ -29,94 +31,113 @@
                                 <p>
                                     Home
                                     <span class="right badge badge-danger"></span>
+                                </p>
+                            </a>
+                        </li>
+
+                        <div class="container">
+                            <li class="nav-item">
+                                <a href="#" class="nav-link">
+                                    <i class="nav-icon fa fa-car"></i>
+                                    <p>
+                                        Car info/insert
+                                        <i class="fas fa-angle-left right"></i>
+                                        <span class="badge badge-info right"></span>
                                     </p>
                                 </a>
-                            </li>
+                                <ul class="nav nav-treeview">
+                                    <li class="nav-item">
+                                        <a href="car_info.php" class="nav-link">
+                                            <i class="far fa-circle nav-icon"></i>
+                                            <p>Cars Information</p>
+                                        </a>
+                                    </li>
+                                    <li class="nav-item">
+                                        <a href="car_insert.php" class="nav-link">
+                                            <i class="far fa-circle nav-icon"></i>
+                                            <p>Car Insert</p>
+                                        </a>
+                                    </li>
+                        </div>
 
-                            <li class="nav-item">
-                            <a href="car_info.php" class="nav-link">
-                                <i class="nav-icon fa fa-car"></i>
-                                <p>
-                                    Car Info
-                                    <span class="right badge badge-danger"></span>
-                                    </p>
-                                </a>
-                            </li>
 
-                            <li class="nav-item">
+
+                        <li class="nav-item">
                             <a href="#" class="nav-link">
                                 <i class="nav-icon fa fa-address-card"></i>
                                 <p>
                                     Booking Car Info
                                     <span class="right badge badge-danger"></span>
-                                    </p>
-                                </a>
-                            </li>
+                                </p>
+                            </a>
+                        </li>
+                  
 
-                            <li class="nav-item">
+                        <li class="nav-item">
                             <a href="#" class="nav-link">
                                 <i class="nav-icon fa fa-users"></i>
                                 <p>
                                     Client/Login Info
                                     <span class="right badge badge-danger"></span>
-                                    </p>
-                                </a>
-                            </li>
+                                </p>
+                            </a>
+                        </li>
 
-                            <li class="nav-item">
+                        <li class="nav-item">
                             <a href="#" class="nav-link">
                                 <i class="nav-icon fa fa-street-view"></i>
                                 <p>
                                     Driver Insert
                                     <span class="right badge badge-danger"></span>
-                                    </p>
-                                </a>
-                            </li>
+                                </p>
+                            </a>
+                        </li>
 
-                            <li class="nav-item">
+                        <li class="nav-item">
                             <a href="#" class="nav-link">
-                            <i class="nav-icon fa fa-archive"></i>
+                                <i class="nav-icon fa fa-archive"></i>
                                 <p>
                                     Payment Details
                                     <span class="right badge badge-danger"></span>
-                                    </p>
-                                </a>
-                            </li>
-                        
-                            <li class="nav-item">
+                                </p>
+                            </a>
+                        </li>
+
+                        <li class="nav-item">
                             <a href="#" class="nav-link">
                                 <i class="nav-icon fa fa-comments"></i>
                                 <p>
                                     Feedback
                                     <span class="right badge badge-danger"></span>
-                                    </p>
-                                </a>
-                            </li>
+                                </p>
+                            </a>
+                        </li>
 
                         <li class="nav-item has-treeview">
                             <a href="#" class="nav-link">
                                 <i class="nav-icon far fa-plus-square"></i>
-                                    <p>
-                                        Web Setting
-                                        <i class="fas fa-angle-left right"></i>
-                                    </p>
-                                </a>
-                                    
-                                <ul class="nav nav-treeview">
-                                    <li class="nav-item">
-                                        <a href="#" class="nav-link">
+                                <p>
+                                    Web Setting
+                                    <i class="fas fa-angle-left right"></i>
+                                </p>
+                            </a>
+
+                            <ul class="nav nav-treeview">
+                                <li class="nav-item">
+                                    <a href="#" class="nav-link">
                                         <i class="far fa-circle nav-icon"></i>
                                         <p>Header</p>
-                                        </a>
-                                    </li>
-                                    <li class="nav-item">
-                                        <a href="#" class="nav-link">
+                                    </a>
+                                </li>
+                                <li class="nav-item">
+                                    <a href="#" class="nav-link">
                                         <i class="far fa-circle nav-icon"></i>
-                                            <p>Footer</p>
-                                            </a>
-                                        </li>
-                                    </ul>
-                            </li>
+                                        <p>Footer</p>
+                                    </a>
+                                </li>
+                            </ul>
+
+                        </li>
                         </li>
                     </ul>
                 </nav>
@@ -124,6 +145,7 @@
             </div>
             <!-- /.sidebar -->
         </aside>
-</div>
+    </div>
 </body>
+
 </html>
