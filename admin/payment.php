629d91d88def5189468ce7a34b1fca9c8d388b70
tejasgund02 <tejasgund02@gmail.com>
2022-04-14 15:34:51 +0530

    ##.. creating initial payment page.

---
+++ admin/payment.php
@@ -0,0 +1,81 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <?php
+    include "header.php";
+    ?>
+</head>
+
+<body class="hold-transition sidebar-mini layout-fixed">
+    <div class="wrapper">
+        <?php
+        include "leftmeun.php";
+        ?>
+        <!-- Content Wrapper. Contains page content -->
+        <div class="content-wrapper">
+            <!-- Content Header (Page header) -->
+            <div class="content-header">
+                <div class="container-fluid">
+                    <div class="row mb-2">
+                        <div class="col-sm-6">
+                            <h1 class="m-0 text-dark">Payment Details</h1>
+                        </div><!-- /.col -->
+                        <div class="col-sm-6">
+                            <!-- <ol class="breadcrumb float-sm-right">
+                <li class="breadcrumb-item"><a href="index1.php">Car Insert Info</a>/Home</li>
+
+              </ol> -->
+                        </div><!-- /.col -->
+                    </div><!-- /.row -->
+
+                    <!-- btn btn-block bg-gradient-secondary btn-lg -->
+                </div><!-- /.container-fluid -->
+            </div>
+            <!-- /.content-header -->
+
+            <!-- Main content -->
+            <table class="table">
+                <thead class="table-dark">
+                    <tr>
+                        <th scope="col">#</th>
+                        <th scope="col">Name</th>
+                        <th scope="col">Transition ID</th>
+                        <th scope="col">Phone</th>
+                        <th scope="col">Email</th>
+                        <th scope="col">Date/Time </th>
+                        <th scope="col">Payment Status </th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <tr>
+                        <th scope="row">1</th>
+                        <td>Tejas Gund</td>
+                        <td>TD9561409792</td>
+                        <td>tejasgund02@gmail.com</td>
+                        <td>9561409792</td>
+                        <td>20/07/2001 12.34 PM</td>
+                        <td>Done</td>
+                    </tr>
+                    <tr>
+                        <th scope="row">2</th>
+                        <td>Sanket</td>
+                        <td>TD140979ss2</td>
+                        <td>Sanekt7373@gmail.com</td>
+                        <td>95634234</td>
+                        <td>20/07/2022 12.34 PM</td>
+                        <td>Pending</td>
+                    </tr>
+                    </tr>
+                </tbody>
+            </table>
+
+            <!-- Main Content End -->
+        </div>
+        <?php
+        include "footer.php";
+        ?>
+    </div>
+</body>
+
+</html>
