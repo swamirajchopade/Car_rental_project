e943ed876ad70df3ca3139474a35c63dd28f7e94
tejasgund02 <tejasgund02@gmail.com>
2022-05-05 23:32:08 +0530

    #.chenging title of admin page.

--- admin/index.php
+++ admin/index.php
@@ -30,7 +30,7 @@ if (isset($_SESSION["username"])) {
 
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <title>AdminLTE 3 | Log in</title>
+    <title>Admin Login | Car rental System</title>
 
     <!-- Google Font: Source Sans Pro -->
     <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,400i,700&display=fallback">
