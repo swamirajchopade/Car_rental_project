c463cc97c02b65698deb90a98f80a6f1df860727
sanketnalage <sanketnalage88@gmail.com>
2022-05-05 18:04:51 +0530

    #..Edit driver has done well..
    It is working properly.

--- admin/edit_driver.php
+++ admin/edit_driver.php
@@ -5,36 +5,7 @@ if ($_SESSION["login"] == true) {
     header("Location:index.php");
 }
 ?>
-<?php
-if ($_SERVER['REQUEST_METHOD'] == "POST") {
-    include "config.php";
-    $v_id = $_GET['id'];
-    $dname = $_POST['dname'];
-    $dcontactno = $_POST['dcontactno'];
-    $daltno = $_POST['daltno'];
-    $dgender = $_POST['dgender'];
-    $ddob = $_POST['ddob'];
-    $daddress = $_POST['daddress'];
-    $dstate = $_POST['dstate'];
-    $ddist = $_POST['ddist'];
-    $dpin = $_POST['dpin'];
-    $dbloodgroup = $_POST['dbloodgroup'];
-    $ddlno = $_POST['ddlno'];
-    $dvehicleno = $_POST['dvehicleno'];
 
-    $query = " UPDATE `tbldriver` SET `dname` = '{$dname}', `dcontactno` = '{$dcontactno}', `daltno` = '{$daltno}', `{$dgender}` = '2', `ddob` = '{$ddob}', `daddress` = '{$daddress}', `dstate` = '{$dstate}', `ddist` = '{$ddist}', `dpin` = '{$dpin}', `dbloodgroup` = '{$dbloodgroup}', `ddlno` = '{$ddlno}' WHERE `tbldriver`.`did` ='{$v_id}' ;";
-
-
-    $result = mysqli_query($conn, $query) or die("Query Unsuccessful.");
-    //header("Location:driver_info.php");
-}
-?>
-<?php
-$v_id = $_GET['id'];
-$sql_1 = "SELECT * FROM tbldriver WHERE did = '{$v_id}'";
-$result_1 = mysqli_query($conn, $sql_1) or die("Query Unsuccessful.");
-$row = mysqli_fetch_assoc($result_1);
-?>
 <!DOCTYPE html>
 <html lang="en">
 
@@ -71,18 +42,52 @@ $row = mysqli_fetch_assoc($result_1);
                 </div><!-- /.container-fluid -->
             </div>
             <!-- /.content-header -->
+            <?php
+                if ($_SERVER['REQUEST_METHOD'] == "POST") {
+                    include "config.php";
+
+                    $v_id = $_GET['id'];
+                    $dname = $_POST['dname'];
+                    $dcontactno = $_POST['dcontactno'];
+                    $daltno = $_POST['daltno'];
+                    $dgender = $_POST['dgender'];
+                    $ddob = $_POST['ddob'];
+                    $daddress = $_POST['daddress'];
+                    $dstate = $_POST['dstate'];
+                    $ddist = $_POST['ddist'];
+                    $dpin = $_POST['dpin'];
+                    $dbloodgroup = $_POST['dbloodgroup'];
+                    $ddlno = $_POST['ddlno'];
+                    $dvehicleno = $_POST['dvehicleno'];
+
+                    $query = " UPDATE tbldriver SET dname = '{$dname}', dcontactno = '{$dcontactno}', daltno = '{$daltno}', dgender = '{$dgender}', ddob = '{$ddob}', daddress = '{$daddress}', dstate = '{$dstate}', ddist = '{$ddist}', dpin = '{$dpin}', dbloodgroup = '{$dbloodgroup}', ddlno = '{$ddlno}' WHERE tbldriver.did ='{$v_id}'";
+
+
+                    $result = mysqli_query($conn, $query) or die("Query Unsuccessful.");
+                    //header("Location:driver_info.php");
+                }
+            ?>
+
+            <?php
+            include 'config.php';
+
+            $v_id = $_GET['id'];
+            $sql_1 = "SELECT * FROM tbldriver WHERE did = '{$v_id}'";
+            $result_1 = mysqli_query($conn, $sql_1) or die("Query Unsuccessful.");
+            $row = mysqli_fetch_assoc($result_1);
+            ?>
 
             <!-- Main content -->
             <div class="container m-1">
                 <form class="row g-3" method="post" action="<?php $_SERVER['PHP_SELF']; ?>" enctype="multipart/form-data">
                     <div class="col-md-6">
                         <label for="inputEmail4" class="form-label">Full Name<span style="color:red">*</span></label>
-                        <input type="hidden" name="vid" value="<?php echo $row['did']; ?>" />
-                        <input type="text" class="form-control" name="dname" value="<?php echo $row['dname']; ?>" required>
+                        <input type="hidden" name="did" value="<?php echo $row['did']; ?>">
+                        <input type="text" class="form-control" name="dname" value="<?php echo $row['dname']; ?>">
                     </div>
                     <div class="col-md-6">
                         <label for="inputPassword4" class="form-label">Mobile No.<span style="color:red">*</span></label>
-                        <input type="text" class="form-control" name="dcontactno" value="<?php echo $row['dcontactno']; ?>" required>
+                        <input type="text" class="form-control" name="dcontactno" value="<?php echo $row['dcontactno']; ?>">
                     </div>
                     <!-- Date -->
                     <div class="col-md-6">
@@ -91,8 +96,8 @@ $row = mysqli_fetch_assoc($result_1);
                             <div class="input-group-prepend">
                                 <span class="input-group-text"><i class="far fa-calendar-alt"></i></span>
                             </div>
-                            <input type="text" class="form-control" name="ddob" data-inputmask-alias="datetime" data-inputmask-inputformat="dd/mm/yyyy" data-mask value="<?php echo $row['ddob']; ?>" required>
-                        </div>
+                            <input type="text" class="form-control" name="ddob" data-inputmask-alias="datetime" data-inputmask-inputformat="dd/mm/yyyy" data-mask value="<?php echo $row['ddob']; ?>">
+                            </div>
                     </div>
                     <!-- date end -->
                     <div class="col-md-6">
@@ -105,41 +110,41 @@ $row = mysqli_fetch_assoc($result_1);
                     </div>
                     <div class="col-md-6">
                         <label for="inputAddress" class="form-label">Address<span style="color:red">*</span></label>
-                        <textarea class="form-control" name="daddress" rows="1" value="<?php echo $row['daddress']; ?>" required=""></textarea>
+                        <textarea class="form-control" name="daddress" rows="1" value="<?php echo $row['daddress']; ?>"></textarea>
                     </div>
                     <div class="col-md-6">
                         <label for="inputState" class="form-label">State<span style="color:red">*</span></label>
-                        <input type="text" class="form-control" name="dstate" value="<?php echo $row['dstate']; ?>" required>
+                        <input type="text" class="form-control" name="dstate" value="<?php echo $row['dstate']; ?>">
                     </div>
                     <div class="col-md-6">
                         <label for="indputdist" class="form-label">Dist<span style="color:red">*</span></label>
-                        <input type="text" class="form-control" name="ddist" value="<?php echo $row['ddist']; ?>" required>
+                        <input type="text" class="form-control" name="ddist" value="<?php echo $row['ddist']; ?>">
                     </div>
                     <div class="col-md-6">
                         <label for="citypin" class="form-label">Pin<span style="color:red">*</span></label>
-                        <input type="text" class="form-control" name="dpin" value="<?php echo $row['dpin']; ?>" required>
+                        <input type="text" class="form-control" name="dpin" value="<?php echo $row['dpin']; ?>">
                     </div>
                     <div class="col-md-6">
                         <label for="inputCity" class="form-label">Blood Group<span style="color:red">*</span></label>
-                        <input type="text" class="form-control" name="dbloodgroup" value="<?php echo $row['dbloodgroup']; ?>" required>
+                        <input type="text" class="form-control" name="dbloodgroup" value="<?php echo $row['dbloodgroup']; ?>">
                     </div>
                     <div class="col-md-6">
                         <label for="year" class="form-label">Driving Lic. NO.<span style="color:red">*</span></label>
-                        <input type="text" class="form-control" name="ddlno" value="<?php echo $row['ddlno']; ?>" required>
+                        <input type="text" class="form-control" name="ddlno" value="<?php echo $row['ddlno']; ?>">
                     </div>
                     <div class="col-md-6">
                         <label for="capacity" class="form-label">Releted Vehicle no.<span style="color:red">*</span></label>
-                        <input type="text" class="form-control" name="dvehicleno" value="<?php echo $row['dvehicleno']; ?>" required>
+                        <input type="text" class="form-control" name="dvehicleno" value="<?php echo $row['dvehicleno']; ?>">
                     </div>
                     <div class="col-md-6">
-                        <label for="capacity" class="form-label">alter phone no.<span style="color:red">*</span></label>
-                        <input type="text" class="form-control" name="daltno" value="<?php echo $row['daltno']; ?>" required>
+                        <label for="capacity" class="form-label">Alter phone no.<span style="color:red">*</span></label>
+                        <input type="text" class="form-control" name="daltno" value="<?php echo $row['daltno']; ?>">
                     </div>
                     <div class="hr-dashed"></div>
 
 
                     <div class="col-12">
-                        <button type="submit" class="btn btn-primary">Insert driver</button>
+                        <button type="submit" class="btn btn-primary"><a href="driver_info.php" style="color: white">Update Driver</a></button>
                     </div>
                 </form>
             </div>
